35553e2cc87926d1a51cb04dab4a3535ad4c88ec
Craig Reilly <craigfreilly@gmail.com>
2024-04-04 17:30:59 +0100

    Added text summary of progress

--- data/progress.go
+++ data/progress.go
@@ -56,7 +56,7 @@ func Run() {
 type tickMsg time.Time
 
 func (p Progress) Init() tea.Cmd {
-	return tickCmd()
+	return p.tickCmd()
 }
 
 func (m Progress) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -80,7 +80,7 @@ func (m Progress) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		// percentage value explicitly, too.
 		//cmd := m.progress.IncrPercent(0.25)
 		cmd := m.progress.SetPercent(m.goalPercentage())
-		return m, tea.Batch(tickCmd(), cmd)
+		return m, tea.Batch(m.tickCmd(), cmd)
 
 	// FrameMsg is sent when the progress bar wants to animate itself
 	case progress.FrameMsg:
@@ -93,15 +93,41 @@ func (m Progress) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	}
 }
 
+func (p Progress) amountString(pad string) string {
+    raised,err := strconv.Atoi(p.fundraiser.Raised)
+    if err != nil {
+        return "\n\n"
+    }
+    supporters := p.fundraiser.Supporters
+    supporterors := "supporters"
+    if supporters == 1 {
+        supporterors = "supporter"
+    }
+    return fmt.Sprintf("%s$%.2f USD\n%sRaised by %d %s\n", pad, float64(raised)/100.00, pad, supporters, supporterors)
+}
+
+func (p Progress) goalString(pad string) string {
+    goal,err := strconv.Atoi(p.fundraiser.GoalAmount)
+    if err != nil {
+        return "\n\n"
+    }
+    return fmt.Sprintf("$%.2f USD%s\nGoal%s\n", float64(goal)/100.00, pad, pad)
+}
+
 func (m Progress) View() string {
 	pad := strings.Repeat(" ", padding)
-	return "\n" +
+    raisedSummaryStyle := lipgloss.NewStyle().Width(m.progress.Width/2).Align(lipgloss.Left)
+    goalSummaryStyle := lipgloss.NewStyle().Width(m.progress.Width/2).Align(lipgloss.Right)
+    progressSummary := lipgloss.JoinHorizontal(lipgloss.Center, raisedSummaryStyle.Render(m.amountString(pad)), goalSummaryStyle.Render(m.goalString(pad+" ")))
+    return "\n" +
+        progressSummary + "\n" +  
 		pad + m.progress.View() + "\n\n" +
 		pad + helpStyle("Press any key to quit")
 }
 
-func tickCmd() tea.Cmd {
-	return tea.Tick(time.Second*1, func(t time.Time) tea.Msg {
+func (p Progress) tickCmd() tea.Cmd {
+    p.fundraiser = db.GetFundraiserData()
+	return tea.Tick(time.Second*5, func(t time.Time) tea.Msg {
 		return tickMsg(t)
 	})
 }
