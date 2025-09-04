385ea5c8bccf71ea08dcc163262b807ba0270696
Craig Reilly <craigfreilly@gmail.com>
2025-09-05 00:12:29 +0100

    Add config file for changable text and QR code for link

--- data/progress.go
+++ data/progress.go
@@ -8,17 +8,22 @@ import (
 	"github.com/charmbracelet/bubbles/progress"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
+	"github.com/spf13/viper"
 	"github.com/finitelycraig/nethackathon-charity-progress/db"
+	"github.com/skip2/go-qrcode"
 )
 
 type Progress struct {
 	fundraiser db.Fundraiser
+	config	   viper.Viper
 	width      int
 	height     int
 	progress   progress.Model
+	qr	   string
 	showInfo   bool
 	showLink   bool
 	showBitly  bool
+	showQR     bool
 }
 
 func (p Progress) goalPercentage() float64 {
@@ -35,7 +40,19 @@ func (p Progress) goalPercentage() float64 {
 }
 
 func NewProgress() Progress {
-	return Progress{fundraiser: db.GetFundraiserData(), progress: progress.New(progress.WithDefaultGradient())}
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("fatal error config file: %w", err))
+	}
+	if _, ok := viper.Get("tagline").(string); !ok {
+		panic(fmt.Errorf("tagline not set as string in config.yaml"))
+	} else if _, ok = viper.Get("link").(string); !ok {
+		panic(fmt.Errorf("link not set as string in config.yaml"))
+	}
+	return Progress{fundraiser: db.GetFundraiserData(), progress: progress.New(progress.WithDefaultGradient()), qr: generateQR(viper.Get("link").(string))}
 }
 
 const (
@@ -67,6 +84,8 @@ func (m Progress) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				m.showLink = !m.showLink
 			case "b":
 				m.showBitly = !m.showBitly
+			case "c":
+				m.showQR = !m.showQR
 			}
 		}
 
@@ -84,7 +103,6 @@ func (m Progress) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		cmd := m.progress.SetPercent(m.goalPercentage())
 		return m, tea.Batch(m.tickCmd(), cmd)
 
-	// FrameMsg is sent when the progress bar wants to animate itself
 	case progress.FrameMsg:
 		progressModel, cmd := m.progress.Update(msg)
 		m.progress = progressModel.(progress.Model)
@@ -117,21 +135,102 @@ func (p Progress) goalString() string {
 	return fmt.Sprintf("$%.2f USD\nGoal\n", float64(goal)/100.00)
 }
 
+func quarterBlockFromBitmap(upperLeft, upperRight, lowerLeft, lowerRight bool) string {
+	if upperLeft && upperRight && lowerLeft && lowerRight {
+		return "█"
+	}
+	if upperLeft && upperRight && !lowerLeft && !lowerRight {
+		return "▀"
+	}
+	if !upperLeft && !upperRight && lowerLeft && lowerRight {
+		return "▄"
+	}
+	if upperLeft && !upperRight && lowerLeft && !lowerRight {
+		return "▌"
+	}
+	if !upperLeft && upperRight && !lowerLeft && lowerRight {
+		return "▐"
+	}
+	if upperLeft && !upperRight && !lowerLeft && lowerRight {
+		return "▚"
+	}
+	if !upperLeft && upperRight && lowerLeft && !lowerRight {
+		return "▞"
+	}
+	if upperLeft && upperRight && lowerLeft && !lowerRight {
+		return "▛"
+	}
+	if upperLeft && upperRight && !lowerLeft && lowerRight {
+		return "▜"
+	}
+	if upperLeft && !upperRight && lowerLeft && lowerRight {
+		return "▙"
+	}
+	if !upperLeft && upperRight && lowerLeft && lowerRight {
+		return "▟"
+	}
+	if upperLeft && !upperRight && !lowerLeft && !lowerRight {
+		return "▘"
+	}
+	if !upperLeft && upperRight && !lowerLeft && !lowerRight {
+		return "▝"
+	}
+	if !upperLeft && !upperRight && lowerLeft && !lowerRight {
+		return "▖"
+	}
+	if !upperLeft && !upperRight && !lowerLeft && lowerRight {
+		return "▗"
+	}
+	if !upperLeft && !upperRight && !lowerLeft && !lowerRight {
+		return " "
+	}
+	return ""
+}
+
+func generateQR(link string) string{
+	qr, err := qrcode.New(link, qrcode.Medium)
+	if err != nil {
+		return "Unable to generate QR code"
+	}
+	var output string
+	for i := 3; i < len(qr.Bitmap()) - 3; i+=2 {
+		for j := 3; j < len(qr.Bitmap()) -3; j+=2 {
+			upperLeft := qr.Bitmap()[i][j]
+			upperRight := qr.Bitmap()[i][j+1]
+			lowerLeft := qr.Bitmap()[i+1][j]
+			lowerRight := qr.Bitmap()[i+1][j+1]
+			output += quarterBlockFromBitmap(upperLeft, upperRight, lowerLeft, lowerRight)
+		}
+		output += "\n"
+	}
+	return output
+}
+
 func (m Progress) View() string {
 	raisedSummaryStyle := lipgloss.NewStyle().Width(m.progress.Width / 2).Align(lipgloss.Left)
 	goalSummaryStyle := lipgloss.NewStyle().Width(m.progress.Width / 2).Align(lipgloss.Right)
 	progressSummary := lipgloss.JoinHorizontal(lipgloss.Center, raisedSummaryStyle.Render(m.amountString()), goalSummaryStyle.Render(m.goalString()))
 	progressBar := m.progress.View() //+ "\n\n" + infoStyle("Press ctl+c or q or Esc to quit")
+	var view string
 	if m.showInfo {
-		progressBar += "\n\n" + infoStyle("NetHackathon VI Fundraising for GiveDirectly!")
+		if tagline, ok := viper.Get("tagline").(string); ok {
+			progressBar += "\n\n" + infoStyle(tagline)
+		}
 		if m.showLink && !m.showBitly {
-			progressBar += "\n" + infoStyle("https://www.every.org/givedirectly/f/nethackathon-vi-fundraising")
+			if link, ok := viper.Get("link").(string); ok {
+				progressBar += "\n" + infoStyle(link)
+			}
 		} else if m.showLink && m.showBitly {
-			progressBar += "\n" + infoStyle("https://bit.ly/nh-givedirectly")
-
+			if shortlink, ok := viper.Get("shortlink").(string); ok {
+				progressBar += "\n" + infoStyle(shortlink)
+			}
 		}
 	}
-	view := lipgloss.JoinVertical(lipgloss.Center, progressSummary, progressBar)
+	if m.showQR {
+		view = infoStyle(m.qr)
+	} else {
+		view = lipgloss.JoinVertical(lipgloss.Center, progressSummary, progressBar)
+	}
 
 	return lipgloss.Place(m.width, m.height, lipgloss.Center, lipgloss.Center, view)
 }
